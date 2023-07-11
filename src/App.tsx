a9c6564c7d69e7e2f1abbb736c8111c9bcbf6a50
ruranjo <rubens.rangel.job@gmail.com>
2023-07-11 13:24:51 -0400

    fetch request and responsive done with react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,47 @@
-
+import {useEffect, useState} from 'react'
+import { useQuery } from 'react-query'
 import './App.css'
+import { Drawer, LinearProgress, Grid, Badge } from '@mui/material'
+import AddShoppingCartIcon  from '@mui/icons-material/AddShoppingCart';
+import MailIcon from '@mui/icons-material/Mail';
+import { Wrapper } from './App.styles';
 
-function App() {
+export type CartItemType = {
+  id: number;
+  title:string;
+  price:number;
+  category:string;
+  description:string;
+  image:string;
+  amount:number;
+}
+
+const getProducts = async (): Promise<CartItemType[]> => {
+  return await (await fetch('https://fakestoreapi.com/products')).json()
+}
+
+
+
+const App = () => {
+  const {data, isLoading, error} = useQuery<CartItemType[]>('products', getProducts);
+  console.log(data)
+
+  const getTotalItems = () => null;
+  const handleAddToCard = () => null;
+  const handleRemoveFromCart = () => null;
+
+  console.log(isLoading)
+  if(isLoading) return <LinearProgress />
+  if(error) return <div>something went wrong... </div>
 
   return (
     <>
-      hola mundo
+      <p>holallala</p>
+      
+      <Drawer> </Drawer>
+      <Badge badgeContent={4} color="primary">
+        <MailIcon color="action" />
+      </Badge>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+// import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {QueryClient, QueryClientProvider } from 'react-query'
+
+const client = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+<QueryClientProvider client={client}>
+  <App />
+</QueryClientProvider>,)
